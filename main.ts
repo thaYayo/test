0a00f4990e94b74ccf52180b8904bf6409c9b0d6
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 11:09:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let Players = ["JonasB", "JonasW", "Lionel"]
+// Loopa y
+for (let index = 0; index < 100; index++) {
+    // loopa x
+    for (let index = 0; index < 100; index++) {
+        tiles.setCurrentTilemap(tilemap`level2`)
+    }
+}
