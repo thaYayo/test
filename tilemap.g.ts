1c173112ddf4552feef1d944bf635b8bc274a48c
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 11:14:34 +0200

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -6,6 +6,8 @@ namespace myTiles {
     export const tile2 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -58,6 +60,8 @@ namespace myTiles {
             case "Rock":
             case "tile2":return tile2;
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile3":return tile3;
         }
         return null;
     })
