7ead8f96cd4b288598f63bc188214fdf65ba6a1e
thaYayo <153025998+thaYayo@users.noreply.github.com>
2024-08-23 11:28:03 +0200

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -49,6 +49,25 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
+            case "level":
+            case "level3":return tiles.createTilemap(hex`100010000c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0d0d0d0d0d0d0d0d0d0d0d0d0d0c0c101010101010101010101010100d0c0c0d01020202020202020202030b0d0c0c0d08090909090909090909040b0d0d0c0d08090909090909090909040b0a0d0c0d08090909090909090909040b0d0c0c0d08090909090909090909040b0d0c0c0d08090909090909090909040b0d0c0c0d08090909090909090909040b0d0c0c0d08090909090909090909040e0d0c0c0d08090909090909090909040e0d0c0c0d07060606060606060606050e0d0c0c0d0d0e0e0e0e0e0e0e0e0e0e0f0d0c0c0d0d0d0d0d0a0a0a0a0a0a0d0d0d0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath9,sprites.castle.tilePath8,sprites.castle.tilePath7,sprites.castle.tilePath4,sprites.castle.tilePath5,sprites.swamp.swampTile1,sprites.builtin.forestTiles20,sprites.builtin.forestTiles4,sprites.swamp.swampTile2,sprites.builtin.forestTiles12,sprites.builtin.forestTiles19,sprites.builtin.forestTiles21], TileScale.Sixteen);
         }
         return null;
     })
